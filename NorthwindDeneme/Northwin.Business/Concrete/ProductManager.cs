308abb08c5e8791de34a9c92fbbeb2143b480385
İbrahim Enes Çiftçi <ciftcienes23@hotmail.com>
2019-01-09 22:23:56 +0300

    enes çiftçi

---
+++ NorthwindDeneme/Northwin.Business/Concrete/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.DataAccess.Concrete;
+
+namespace Northwin.Business.Concrete
+{
+   public class ProductManager
+   {
+       private ProductDal _product = new ProductDal();
+        public List<Product> GetAll()
+        {
+            return _product.GetAll();
+        }
+    }
+}
